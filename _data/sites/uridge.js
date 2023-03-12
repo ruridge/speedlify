c61f5228501d425b992bc32693da6707d0d4a359
Robert Uridge <856071+ruridge@users.noreply.github.com>
2023-03-13 00:35:46 +0800

    Build uridge output, remove demo site

--- _data/sites/uridge.js
+++ _data/sites/uridge.js
@@ -3,9 +3,10 @@ module.exports = {
   description: "Rob’s Personal web site",
   // skip if localhost
   // skip if this is a new fork of the speedlify (not Zach’s)
-  skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+  // skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
   options: {
-    frequency: 60 * 23, // 23 hours
+    runs: 1,
+    frequency: 1, // (im minutes)
     // Use "run" if the sites don’t share assets on the same origin
     //           and we can reset chrome with each run instead of
     //           each site in every run (it’s faster)
